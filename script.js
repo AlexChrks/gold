76eb0ae81e04bc619b6326696352817921035f15
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-05-11 22:33:24 +0300

    fix: change api link

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-let reserve = {
-  base: "EUR",
-  date: "2021-05-04",
-  rates: {
-    AUD: 1.5608,
-    BGN: 1.9558,
-    BRL: 6.5785,
-    CAD: 1.4836,
-    CHF: 1.098,
-    CNY: 7.8136,
-    CZK: 25.841,
-    DKK: 7.4361,
-    GBP: 0.8677,
-    HKD: 9.3374,
-    HRK: 7.5408,
-    HUF: 360.31,
-    IDR: 17395.47,
-    ILS: 3.9097,
-    INR: 88.814,
-    ISK: 148.8,
-    JPY: 131.26,
-    KRW: 1353.56,
-    MXN: 24.404,
-    MYR: 4.9563,
-    NOK: 10.0123,
-    NZD: 1.6887,
-    PHP: 57.787,
-    PLN: 4.5584,
-    RON: 4.9275,
-    RUB: 90.2438,
-    SEK: 10.1673,
-    SGD: 1.6073,
-    THB: 37.475,
-    TRY: 10.011,
-    USD: 1.2021,
-    ZAR: 17.4433,
-  }
-}
-
 const wrapper = document.querySelector('.global-wrapper');
 
 function showErrorNotification() {
@@ -51,7 +12,7 @@ function showErrorNotification() {
 }
 
 async function query() {
-  const url = 'https://api.ratesapi.io/api/latests';
+  const url = 'https://api.ratesapi.io/api/latest';
   let response = await fetch(url);
   return response;
 }
