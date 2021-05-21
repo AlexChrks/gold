bf086d4df3d76a0521820c202d57ae414b99684d
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2021-05-21 15:28:20 +0300

    fix: fix error handling

--- src/scripts/query.js
+++ src/scripts/query.js
@@ -1,6 +1,10 @@
+import showErrorNotification from './showErrorNotification.js';
+
 export default async function query(errorParent) {
   const url = 'https://api.ratesapi.io/api/latest';
-  let response = await fetch(url);
+  let response = await fetch(url).catch(() => {
+    showErrorNotification(document.querySelector('.global-wrapper'))
+  });
 
   if (response.status !== 200) {
     const {default: showErrorNotification} = await import('./showErrorNotification.js');
